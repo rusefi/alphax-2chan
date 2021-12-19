719a876e538bc7e70feae036591c6a16bf1b54c4
Andrei <andreikagit@users.noreply.github.com>
2021-12-19 19:16:49 +0200

    initial draft kicad project & schematic files

---
+++ step2_copy_alphax_2ch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd hellen-one
+python2.7 ./bin/copy_from_Kicad.py "frames:alphax" "../.." "../../gerber" "2ch" "a"
+
+echo "Done!"
---
+++ step3_create_alphax_2ch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd hellen-one
+sh bin/create_board_with_prefix.sh "alphax" ".." "2ch" "a" "bom_replace_alphax-2ch-a.csv " "0,0"
