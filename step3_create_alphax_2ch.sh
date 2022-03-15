c59640b55d380aa999292963345814ac418fd2ab
andreika-git <andrei.home@gmail.com>
2022-03-15 09:05:10 +0200

    fix pulldowns & qr https://github.com/rusefi/alphax-2chan/issues/40 https://github.com/rusefi/alphax-2chan/issues/36 https://github.com/rusefi/alphax-2chan/issues/37

--- step3_create_alphax_2ch.sh
+++ step3_create_alphax_2ch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board_with_prefix.sh "alphax_" ".." "2ch" "b" "bom_replace_alphax-2ch-b.csv " "0,0"
+sh bin/create_board_with_prefix.sh "alphax_" ".." "2ch" "c" "bom_replace_alphax-2ch-c.csv " "0,0"
