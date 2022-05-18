80d14ff8179fb9a7c1ce814eebfca62415b83382
rusefillc <sdfsdfqsf2334234234>
2022-05-18 09:57:26 -0400

    step3 fails on Debian fix #57

--- step3_create_board.sh
+++ step3_create_board.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -f hellen-one/git_scripts ]; then
+    echo "No submodules?"
+    git submodule update --init --recursive
+fi
+
 cd hellen-one
 sh bin/create_board_with_prefix.sh "alphax_" ".." "2ch" "c" "bom_replace_alphax-2ch-c.csv " "0,4"
