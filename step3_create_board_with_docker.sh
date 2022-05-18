7b87e7bb00ba92284ece0bde7cd8459fdaad8c2e
zhoob2004 <zhoob2004@users.noreply.github.com>
2022-05-18 14:09:04 -0700

    added docker versions of build scripts

---
+++ step3_create_board_with_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f hellen-one/git_scripts ]; then
+    echo "No submodules?"
+    git submodule update --init --recursive
+fi
+
+# See step2_copy_with_docker.sh for explanation of the following command
+
+docker run --rm -it --entrypoint bash -v "$(pwd)":/${PWD##*/} hellen-one ./bin/create_board_with_prefix.sh "alphax_" "/${PWD##*/}" "2ch" "c" "bom_replace_alphax-2ch-c.csv " "0,4"
+
