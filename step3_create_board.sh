7f1a7543590cb096e95e5fe1429e9b391bc081cd
andreika-git <andrei.home@gmail.com>
2022-05-21 17:49:08 +0300

    add xvfb to step3

--- step3_create_board.sh
+++ step3_create_board.sh
@@ -5,5 +5,9 @@ if [ ! -f hellen-one/git_scripts ]; then
     git submodule update --init --recursive
 fi
 
+# start virtual framebuffer (should be already installed)
+export DISPLAY=:99.0
+sudo Xvfb :99 -screen 0 640x480x24 &
+
 cd hellen-one
 sh bin/create_board_with_prefix.sh "alphax_" ".." "2ch" "c" "bom_replace_alphax-2ch-c.csv " "0,4"
