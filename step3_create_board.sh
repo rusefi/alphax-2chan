66e2c81fd56a70d95982438d904e679aff1b145d
rusefillc <sdfsdfqsf2334234234>
2022-05-21 11:06:42 -0400

    hiding for now

--- step3_create_board.sh
+++ step3_create_board.sh
@@ -6,8 +6,8 @@ if [ ! -f hellen-one/git_scripts ]; then
 fi
 
 # start virtual framebuffer (should be already installed)
-export DISPLAY=:99.0
-sudo Xvfb :99 -screen 0 640x480x24 &
+#export DISPLAY=:99.0
+#sudo Xvfb :99 -screen 0 640x480x24 &
 
 cd hellen-one
 sh bin/create_board_with_prefix.sh "alphax_" ".." "2ch" "c" "bom_replace_alphax-2ch-c.csv " "0,4"
